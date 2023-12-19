a13ed5fd2f1c5a5fb90cdf5bd36290a48324e7dc
abelhajsalah <abelhajsalah@deloitte.tn>
2023-12-19 22:02:48 +0100

    add auth and training components

--- fitness-tracker/src/app/app.component.ts
+++ fitness-tracker/src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'fitness-tracker';
+
+  onClick(){
+    console.log(`the project ${this.title} has started`);
+  }
 }
